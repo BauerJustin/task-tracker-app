ddff7ba48954ea249dfb432c836a69b4ef49a979
BauerJustin <justin.bauer@mail.utoronto.ca>
2020-08-16 21:48:00 -0600

    Grouped tasks by date

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, View, FlatList } from 'react-native';
+import { StyleSheet, Button, View, FlatList, AsyncStorage } from 'react-native';
 import Header from './components/Header';
 import TaskInput from './components/TaskInput';
 import TaskItem from './components/TaskItem';
@@ -8,9 +8,11 @@ import Colors from './constants/colors';
 export default function App() {
   const [tasks, setTasks] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
+  var dateList = [];
 
   const addTaskHandler = taskAttributes => {
     setTasks(currentTasks => [...currentTasks, { key: Math.random().toString(), value: taskAttributes[0], date: taskAttributes[1] }]);
+    // saveTasks();
     setIsAddMode(false);
   };
 
@@ -24,18 +26,45 @@ export default function App() {
     setIsAddMode(false);
   }
 
+  const checkPreviousDate = date => {
+    if (dateList.includes(date)) {
+      return null;
+    } else {
+      dateList.push(date);
+      return date;
+    }
+  }
+
+  // const saveTasks = async () => {
+  //   try {
+  //     await AsyncStorage.setItem('tasks', tasks);
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+
+  // const fetchTasks = async () => {
+  //   try {
+  //     const value = await AsyncStorage.getItem('tasks');
+  //     if (value !== null) {
+  //       console.log(value);
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 
   return (
     <View style={styles.screen}>
-      <Header title="Task Tracker"/>
+      <Header title="Task Tracker" />
       <View style={styles.content}>
         <Button title="Add New Task" onPress={() => setIsAddMode(true)} color={Colors.primary} />
-        <FlatList 
-          data={tasks.sort((a, b) => a.date.localeCompare(b.date))} 
-          renderItem={itemData => <TaskItem onDelete={removeTaskHandle.bind(this, itemData.item.key)} title={itemData.item.value} date={itemData.item.date} />}
+        <FlatList
+          data={tasks.sort((a, b) => a.date.localeCompare(b.date))}
+          renderItem={itemData => <TaskItem onDelete={removeTaskHandle.bind(this, itemData.item.key)} title={itemData.item.value} date={checkPreviousDate(itemData.item.date)} />}
         />
       </View>
-      <TaskInput visible={isAddMode} onAddTask={addTaskHandler} onCancel={cancelTaskAdditionHandler}/>
+      <TaskInput visible={isAddMode} onAddTask={addTaskHandler} onCancel={cancelTaskAdditionHandler} />
     </View>
   );
 }
@@ -46,8 +75,8 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.base,
   },
   content: {
-    padding: 10, 
-    justifyContent: 'space-between', 
+    padding: 10,
+    justifyContent: 'space-between',
     alignItems: 'center'
   }
 });
--- components/TaskItem.js
+++ components/TaskItem.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../constants/colors';
+import moment from 'moment';
 
 const TaskItem = props => {
+
+    const [date, setDate] = useState();
+
+    if (date == null && props.date != null){
+        setDate(<Text style={styles.date}>{"  " + moment(props.date).format('dddd MMM Do')}</Text>);
+    }
+
     return (
-        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this, props.key)}>
-            <View style={styles.listItem} >
-               <Text>{props.title}</Text>
-               <Text>{props.date}</Text>
-          </View>
-        </TouchableOpacity>
+        <View>
+            {date}
+            <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this, props.key)}>
+                <View style={styles.listItem} >
+                    <Text style={styles.title}>{props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+
     );
 };
 
@@ -21,8 +32,18 @@ const styles = StyleSheet.create(
             borderColor: Colors.accent,
             borderWidth: 1,
             margin: 5,
-            width: 350
+            width: 350,
+            flexDirection: 'row'
         },
+        title: {
+            color: Colors.base,
+            fontSize: 18,
+            flex: 1,
+        },
+        date: {
+            color: Colors.primary,
+            fontSize: 18,
+        }
     }
 );
 
